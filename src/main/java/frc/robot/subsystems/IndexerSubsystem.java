ded42371f1493878c3ca1ae4d57eedcd69f9d314
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2022-02-10 17:21:34 -0800

    Adding Indexer Motor

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -1,23 +1,24 @@
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 import frc.robot.commands.indexer.EnsureResidualBeltCountdownIsNotRunning;
 import frc.robot.lib.colorwheel.ColorWheelUtils;
 import frc.robot.lib.colorwheel.WheelColors;
-import frc.robot.Constants;
 
 import java.util.function.BooleanSupplier;
 
 import static frc.robot.Constants.MechanismConstants.indexerBeltMotorPort;
+import static frc.robot.Constants.MechanismConstants.indexerMecanumMotorPort;
 
 
 public class IndexerSubsystem extends SubsystemBase {
     // Configure motors and booleans
-    //private final TalonFX indexerMotor = new TalonFX(indexerMecanumMotorPort);
-    private final CANSparkMax beltMotor = new CANSparkMax(indexerBeltMotorPort, CANSparkMaxLowLevel.MotorType.kBrushless);
+    private final TalonFX indexerMotor = new TalonFX(indexerMecanumMotorPort);
+    private final TalonFX beltMotor = new TalonFX(indexerBeltMotorPort);
     private final ColorWheelUtils colorWheelUtils = new ColorWheelUtils();
 
     public WheelColors currentColor = WheelColors.GREEN;
@@ -32,24 +33,25 @@ public class IndexerSubsystem extends SubsystemBase {
     }
 
     public void startIndexer() { // Enables indexer
-        //indexerMotor.set(ControlMode.PercentOutput, Constants.MechanismConstants.indexerSpeed);
+        indexerMotor.set(ControlMode.PercentOutput, Constants.MechanismConstants.indexerSpeed);
         indexerRunning = true;
     }
 
     public void stopIndexer() { // Disables indexer
-        //indexerMotor.set(ControlMode.PercentOutput, 0);
+        indexerMotor.set(ControlMode.PercentOutput, 0);
         indexerRunning = false;
     }
 
     public void toggleIndexer() { if (indexerRunning) stopIndexer(); else startIndexer(); } // Toggles indexer
 
     public void startBelt() { // Enables belt
-        beltMotor.set(Constants.MechanismConstants.beltSpeed);
+        beltMotor.set(ControlMode.PercentOutput, Constants.MechanismConstants.beltSpeed);
         beltRunning = true; new EnsureResidualBeltCountdownIsNotRunning(this).schedule();
     }
 
     public void stopBelt() { // Disables belt
-        beltMotor.set(0); beltRunning = false;
+        beltMotor.set(ControlMode.PercentOutput, 0);
+        beltRunning = false;
     }
 
     public void toggleBelt() { if (beltRunning) stopBelt(); else startBelt(); } // Toggles belt
