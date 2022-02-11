a93b796cc763be211f8026f551468b7d33506c60
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2022-02-10 17:24:16 -0800

    Added Indexer Motor As Falcon (#8)
    * Fixing Limelight Name
    
    * Adding Indexer Motor

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,7 +58,7 @@ public final class Constants {
         public static final int intakeMotorPort = 6;
         public static final int indexerMecanumMotorPort = 0;  // CHANGE
         public static final int indexerBeltMotorPort = 2;  // CHANGE
-        public static final double indexerSpeed = 0.5;
+        public static final double indexerSpeed = 50;
         // TURRET
         public static final int turretMotorPort = 0;  // CHANGE
         // SHOOTER
@@ -71,7 +71,7 @@ public final class Constants {
         // MAX MECHANISM SPEEDS
         public static final double idleOuttakeSpeed = 0.25;
         public static final double intakeSpeed = 0.3; //0 to 1
-        public static final double beltSpeed = 0.5;
+        public static final double beltSpeed = 50;
         public static final double residualBeltRunTime = 5;
 
         // Climber motor ports ...
