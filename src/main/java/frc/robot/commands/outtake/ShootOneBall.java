f6e7a0a2acd4bb429c002383a61bed9eb6be55cb
JTkeio <jacobtkeio@gmail.com>
2022-02-17 10:49:51 -0800

    i forgor x| Add getting values from the ShooterDataTable to the shooting code and define a static shooter data table in robotContainer

--- src/main/java/frc/robot/commands/outtake/ShootOneBall.java
+++ src/main/java/frc/robot/commands/outtake/ShootOneBall.java
@@ -9,10 +9,11 @@ import frc.robot.commands.intake.DisableIntake;
 import frc.robot.commands.limelight.GuaranteeLimelightData;
 import frc.robot.commands.limelight.GuaranteeLimelightDataEquals;
 import frc.robot.lib.GoalNotFoundException;
+import frc.robot.lib.shooterData.ShooterDataTable;
 import frc.robot.subsystems.*;
 
 public class ShootOneBall extends SequentialCommandGroup {
-    public ShootOneBall(DrivetrainSubsystem drivetrain, IndexerSubsystem indexer, IntakeSubsystem intake, LimelightSubsystem limelight, OuttakeSubsystem outtake) {
+    public ShootOneBall(DrivetrainSubsystem drivetrain, IndexerSubsystem indexer, IntakeSubsystem intake, LimelightSubsystem limelight, OuttakeSubsystem outtake, ShooterDataTable shooterDataTable) {
         addRequirements(drivetrain, indexer, intake, limelight, outtake);
         try {
             addCommands(
@@ -20,9 +21,9 @@ public class ShootOneBall extends SequentialCommandGroup {
                     new DisableDrivetrain(drivetrain),
                     new EnableShooter(outtake),
                     new ParallelDeadlineGroup(new GuaranteeLimelightData(limelight), new ManualAdjustTurret(outtake)),
-                    new SetHoodAngle(outtake, limelight.getLimelightOutputAtIndex(2)),
+                    new SetHoodAngle(outtake, shooterDataTable.getSpecs(limelight.getLimelightOutputAtIndex(0)).getAngle()),
                     new SetTurretActive(outtake, true),
-                    new SetSpecificShooterPower(outtake, limelight.getLimelightOutputAtIndex(3)),
+                    new SetSpecificShooterPower(outtake, shooterDataTable.getSpecs(limelight.getLimelightOutputAtIndex(0)).getPower()),
                     new GuaranteeLimelightDataEquals(limelight, 1, 0),
                     new SetTurretActive(outtake, false),
                     new ShootTopBall(indexer, 0.5),
--- src/main/java/frc/robot/lib/shooterData/ShooterDataTable.java
+++ src/main/java/frc/robot/lib/shooterData/ShooterDataTable.java
@@ -32,7 +32,7 @@ public class ShooterDataTable{
     //gets the desired specs for shooter at a distance, linearly interpolating between the
     //two closest data points.
 
-    private ShooterSpec getSpecs(double distance){
+    public ShooterSpec getSpecs(double distance){
         if(distance < MINDIST || distance >= MAXDIST)	return new ShooterSpec();
         int index = (int) ((distance - MINDIST) * K + 0.005);
         System.out.println("INDEX = " + index);
@@ -59,39 +59,3 @@ public class ShooterDataTable{
 
 }
 
-//Stores launch speed, hood angle, and power as doubles
-class ShooterSpec{
-    private double speed;
-    private double hoodAngle;
-    private double power;
-
-    public ShooterSpec(){
-        this.speed = 0.0;
-        this.hoodAngle = 0.0;
-        this.power = 0.0;
-    }
-
-    public ShooterSpec(double theta, double power){
-        this.speed = 0.0;
-        this.hoodAngle = theta;
-        this.power = power;
-    }
-    // power is constrained between 0 and 1, hoodAngle is constrained between 9 and 40 deg
-    public ShooterSpec(double v0, double theta, double power){
-        this.speed = v0;
-        this.hoodAngle = theta > 40 ? 40 : (theta < 9 ? 9 : theta);
-        this.power = power > 1? 1 : (power < 0 ? 0 : power);
-    }
-    public double getSpeed(){
-        return this.speed;
-    }
-    public double getAngle(){
-        return this.hoodAngle;
-    }
-    public double getPower(){
-        return this.power;
-    }
-    public String toString(){
-        return ("speed = " + speed + " m/s \n" + "hoodAngle = " + hoodAngle + " degrees \n" + " power = " + power + "\n");
-    }
-}
