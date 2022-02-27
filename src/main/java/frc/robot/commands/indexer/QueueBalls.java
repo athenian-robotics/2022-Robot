6ba52c62d152bba8d69a6646b866491ba42332db
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2022-02-27 14:29:17 -0800

    Fixed Intake w/ No Pneumatics

--- src/main/java/frc/robot/commands/indexer/QueueBalls.java
+++ src/main/java/frc/robot/commands/indexer/QueueBalls.java
@@ -1,31 +1,25 @@
 package frc.robot.commands.indexer;
 
-import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.IndexerSubsystem;
-import frc.robot.subsystems.IntakeSubsystem;
 
 
 public class QueueBalls extends CommandBase {
     private final IndexerSubsystem indexerSubsystem;
-    private final IntakeSubsystem intakeSubsystem;
     private long startTime = 0;
 
-    public QueueBalls(IndexerSubsystem indexerSubsystem, IntakeSubsystem intakeSubsystem) {
+    public QueueBalls(IndexerSubsystem indexerSubsystem) {
         this.indexerSubsystem = indexerSubsystem;
-        this.intakeSubsystem = intakeSubsystem;
-        addRequirements(this.indexerSubsystem, this.intakeSubsystem);
+        addRequirements(this.indexerSubsystem);
     }
 
     @Override
     public void execute() {
         if (indexerSubsystem.ballPrimed()){
             indexerSubsystem.startIndexer();
-            intakeSubsystem.startIntakeToIndexerMotor();
     }
         else {
             indexerSubsystem.stopIndexer();
-            intakeSubsystem.stopIntakeToIndexerMotor();
         }
     }
 }
