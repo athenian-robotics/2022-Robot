662dcc51a68da8639e96bef1fb5a0a2030500a22
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2022-03-14 15:36:25 -0700

    Auto Routine 1 and 2

---
+++ src/main/java/frc/robot/commands/auto/AutoRoutine2.java
@@ -0,0 +1,34 @@
+package frc.robot.commands.auto;
+
+
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.auto.AutoRoutine2Contents.AutoRoutine2Part1;
+import frc.robot.commands.auto.AutoRoutine2Contents.AutoRoutine2Part2;
+import frc.robot.commands.intake.ToggleIntake;
+import frc.robot.commands.outtake.ShootOne;
+import frc.robot.commands.outtake.ShootTwo;
+import frc.robot.commands.outtake.TurretTurnToAngle;
+import frc.robot.lib.shooterData.ShooterDataTable;
+import frc.robot.subsystems.*;
+
+public class AutoRoutine2 extends SequentialCommandGroup {
+    public AutoRoutine2(ClimberSubsystem climber, DrivetrainSubsystem drivetrain, IndexerSubsystem indexer, IntakeSubsystem intake, OuttakeSubsystem outtake, LimelightSubsystem limelight, ShooterDataTable shooterDataTable) {
+        addCommands(
+                new AutoRoutine2Part1(drivetrain),
+                new ToggleIntake(intake),
+                new WaitCommand(0.75),
+                new ToggleIntake(intake),
+                new ShootTwo(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable),
+                new AutoRoutine2Part2(drivetrain),
+                new ToggleIntake(intake),
+                new WaitCommand(0.75),
+                new ToggleIntake(intake),
+                new ParallelCommandGroup(
+                        new TurretTurnToAngle(outtake, 0),
+                        new ShootOne(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable)
+                )
+        );
+    }
+}
