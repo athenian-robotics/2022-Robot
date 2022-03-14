892c0ddb87284b7f0e4c8587cd27797ca159caf0
Sentientnapkin <73544597+Sentientnapkin@users.noreply.github.com>
2022-03-14 15:39:52 -0700

    Wrote some comments for autoroutine 1 and 2

--- src/main/java/frc/robot/commands/auto/AutoRoutine1.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutine1.java
@@ -18,11 +18,11 @@ import frc.robot.subsystems.*;
 public class AutoRoutine1 extends SequentialCommandGroup {
     public AutoRoutine1(ClimberSubsystem climber, DrivetrainSubsystem drivetrain, IndexerSubsystem indexer, IntakeSubsystem intake, OuttakeSubsystem outtake, LimelightSubsystem limelight, ShooterDataTable shooterDataTable) {
         addCommands(
-                new AutoRoutine1Part1(drivetrain),
+                new AutoRoutine1Part1(drivetrain), //moves down to ball under starting position
                 new ToggleIntake(intake),
                 new ShootOne(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable),
                 new ToggleIntake(intake),
-                new AutoRoutine1Part2(drivetrain),
+                new AutoRoutine1Part2(drivetrain), //moves left to other ball
                 new ToggleIntake(intake),
                 new WaitCommand(0.75),
                 new ToggleIntake(intake),
@@ -30,11 +30,11 @@ public class AutoRoutine1 extends SequentialCommandGroup {
                         new TurretTurnToAngle(outtake, 78.14),
                         new ShootTwo(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable)
                 ),
-                new AutoRoutine1Part3(drivetrain),
+                new AutoRoutine1Part3(drivetrain), //drives to human player terminal
                 new ToggleIntake(intake),
                 new WaitCommand(1),
                 new ToggleIntake(intake),
-                new AutoRoutine1Part4(drivetrain),
+                new AutoRoutine1Part4(drivetrain), //drives back closer to the goal
                 new ParallelCommandGroup(
                         new TurretTurnToAngle(outtake, -10),
                         new ShootTwo(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable)
--- src/main/java/frc/robot/commands/auto/AutoRoutine2.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutine2.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.auto.AutoRoutine2Contents.AutoRoutine2Part1;
 import frc.robot.commands.auto.AutoRoutine2Contents.AutoRoutine2Part2;
+import frc.robot.commands.auto.AutoRoutine2Contents.AutoRoutine2Part3;
 import frc.robot.commands.intake.ToggleIntake;
 import frc.robot.commands.outtake.ShootOne;
 import frc.robot.commands.outtake.ShootTwo;
@@ -16,15 +17,16 @@ import frc.robot.subsystems.*;
 public class AutoRoutine2 extends SequentialCommandGroup {
     public AutoRoutine2(ClimberSubsystem climber, DrivetrainSubsystem drivetrain, IndexerSubsystem indexer, IntakeSubsystem intake, OuttakeSubsystem outtake, LimelightSubsystem limelight, ShooterDataTable shooterDataTable) {
         addCommands(
-                new AutoRoutine2Part1(drivetrain),
+                new AutoRoutine2Part1(drivetrain), //drives up to ball
                 new ToggleIntake(intake),
                 new WaitCommand(0.75),
                 new ToggleIntake(intake),
                 new ShootTwo(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable),
-                new AutoRoutine2Part2(drivetrain),
+                new AutoRoutine2Part2(drivetrain), //drives to the right to get another ball
                 new ToggleIntake(intake),
                 new WaitCommand(0.75),
                 new ToggleIntake(intake),
+                new AutoRoutine2Part3(drivetrain), //bumps opponent ball and gets into a shooting position
                 new ParallelCommandGroup(
                         new TurretTurnToAngle(outtake, 0),
                         new ShootOne(climber, drivetrain, indexer, intake, outtake, limelight, shooterDataTable)
